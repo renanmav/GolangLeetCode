57ba3aeb1ea55a23f3e1117dc2d9c03e59a88493
Renan Mav <renan.mav@hotmail.com>
2024-09-30 15:04:09 -0300

    manacher algorithm

--- 5-longest-palindromic-substring/longest_palindromic_substring.go
+++ 5-longest-palindromic-substring/longest_palindromic_substring.go
@@ -21,7 +21,7 @@ s consist of only digits and English letters.
 */
 
 func longestPalindrome(s string) string {
-	return approach3(s)
+	return approach4(s)
 }
 
 // Approach 1: Check all substrings
@@ -143,5 +143,45 @@ func approach3(s string) string {
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 func approach4(s string) string {
-	panic("not implemented")
+	sPrime := "#"
+	for _, c := range s {
+		sPrime = sPrime + string(c) + "#"
+	}
+
+	n := len(sPrime)
+	palindromeRadii := make([]int, n)
+	center := 0
+	radius := 0
+
+	for i := 0; i < n; i++ {
+		mirror := 2*center - i
+
+		if i < radius {
+			palindromeRadii[i] = min(radius-i, palindromeRadii[mirror])
+		}
+
+		for i+1+palindromeRadii[i] < n && i-1-palindromeRadii[i] >= 0 && sPrime[i+1+palindromeRadii[i]] == sPrime[i-1-palindromeRadii[i]] {
+			palindromeRadii[i]++
+		}
+
+		if i+palindromeRadii[i] > radius {
+			center = i
+			radius = i + palindromeRadii[i]
+		}
+	}
+
+	maxLength := 0
+	centerIndex := 0
+
+	for i := 0; i < n; i++ {
+		if palindromeRadii[i] > maxLength {
+			maxLength = palindromeRadii[i]
+			centerIndex = i
+		}
+	}
+
+	startIndex := (centerIndex - maxLength) / 2
+	longestPalindrome := s[startIndex : startIndex+maxLength]
+
+	return longestPalindrome
 }
